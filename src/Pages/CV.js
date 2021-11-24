61ae59ca1142f41ffeab02d6293c1aa7f4e15c6c
Matt <matthiasvstephens@gmail.com>
2021-11-24 01:46:15 +0100

    Add CV download button

--- src/Pages/CV.js
+++ src/Pages/CV.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Page from './PageComponents/Page';
+import cv from './assets/Matthias Stephens CV.pdf';
 
 export default function CV() {
-  return <Page head='My CV/Resume' comeIn></Page>;
+  return (
+    <Page head='My CV/Resume' comeIn>
+      <a href={cv} download>
+        <button className='cvBtn'>Download my CV!</button>
+      </a>
+    </Page>
+  );
 }
